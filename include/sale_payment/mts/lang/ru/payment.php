55b3cc9c46e1c8fddc2b2f90af365cf945bd3ea4
mittelmabigkeit <goodnazi1488@gmail.com>
2026-01-20 15:04:57 +0300

    Add files via upload

---
+++ include/sale_payment/mts/lang/ru/payment.php
@@ -0,0 +1,32 @@
+<?
+global $MESS;
+
+$MESS["MTS_DTITLE"] = "МТС";
+$MESS["SHOP_ID"] = "Наименование платежного Терминала (key)";
+$MESS["SHOP_ID_DESCR"] = "";
+$MESS["HOST_CONNECT"] = "URL подключения МТС";
+$MESS["SCID_DESCR"] = "";
+$MESS["SCID_DESCT"] = "";
+$MESS["ORDER_ID"] = "Номер заказа";
+$MESS["ORDER"] = "Заказ";
+$MESS["ORDER_ID_DESCR"] = "";
+$MESS["SHOP_KEY"] = "Пароль для проведения операции";
+$MESS["ANTIFRAUD"] = "Идентификатор платежа в системе МТС";
+$MESS["CUSTOMKEY"] = "Идентификатор Пользователя в системе МТС";
+$MESS["SHOULD_PAY"] = "Сумма заказа";
+$MESS["SHOULD_PAY_DESCR"] = "Сумма к оплате";
+$MESS["ORDER_DATE"] = "Дата создания заказа";
+$MESS["ORDER_DATE_DESCR"] = "";
+$MESS["SESSION_TYPE"] = "Тип сессии ";
+$MESS["SESSION_TYPE_DESCR"] = "Определяет количество стадий платежа ";
+$MESS["SESSION_PAY_TYPE"] = "Pay – одностадийный платеж";
+$MESS["SESSION_BLOCK_TYPE"] = "Block – двухстадийный платеж";
+$MESS["PYM_CHANGE_STATUS_PAY"] = "Автоматически оплачивать заказ при получении успешного статуса оплаты";
+$MESS["PYM_CHANGE_STATUS_PAY_DESC"] = "Y - оплачивать, N - не оплачивать.";
+$MESS["FINAL_URL"] = "Url перехода после завершения платежа";
+$MESS["FINAL_URL_DESC"] = "Доступные параметры: {orderid} - ID заказа, {success} - статус платежа";
+$MESS["PAY"] = "Оплатить";
+$MESS["DELIVERY"] = "Доставка";
+
+$MESS["MTS_DDESCR"] = 'МТС';
+?>
